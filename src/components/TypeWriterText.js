827d2a439bb538028abab25ea61c6bb2a36cb3cc
can <21026424+nzmcno@users.noreply.github.com>
2022-11-03 22:43:27 +0000

    home.js responsive

--- src/components/TypeWriterText.js
+++ src/components/TypeWriterText.js
@@ -1,78 +1,101 @@
-import React from 'react'
-import styled from 'styled-components'
-import Typewriter from 'typewriter-effect';
-import Button from './Button';
+import React from "react";
+import styled from "styled-components";
+import Typewriter from "typewriter-effect";
+import Button from "./Button";
 
+const Title = styled.h2`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: capitalize;
+  width: 80%;
+  color: ${(props) => props.theme.text};
+  align-self: flex-start;
 
-const Title= styled.h2`
-font-size: ${props => props.theme.fontxxl};
-text-transform: capitalize;
-width: 80%;
-color: ${props => props.theme.text};
-align-self: flex-start;
-
-span{
+  span {
     text-transform: uppercase;
-    font-family: Arial , cursive;
-}
-.text-1{
+    font-family: Arial, cursive;
+  }
+  .text-1 {
     color: blue;
-}
-.text-2{
+  }
+  .text-2 {
     color: orange;
-}
-.text-3{
+  }
+  .text-3 {
     color: red;
-}
+  }
+  @media (max-width: 70em) {
+    font-size: ${(props) => props.theme.fontxl};
+  }
+  @media (max-width: 48em) {
+    align-self: center;
+    text-align: center;
+  }
+  @media (max-width: 40em) {
+    width: 90%;
+  }
 `;
 
 const SubTitle = styled.h3`
-font-size: ${props => props.theme.fontlg};
-text-transform: capitalize;
-color: ${props => `rgba(${props.theme.textRgba}, 0.6)`};
-font-weight:600;
-margin-bottom: 1rem;
-width: 80%;
-align-self: flex-start;
+  font-size: ${(props) => props.theme.fontlg};
+  text-transform: capitalize;
+  color: ${(props) => `rgba(${props.theme.textRgba}, 0.6)`};
+  font-weight: 600;
+  margin-bottom: 1rem;
+  width: 80%;
+  align-self: flex-start;
+  @media (max-width: 40em) {
+    font-size: ${(props) => props.theme.fontmd};
+  }
 
+  @media (max-width: 48em) {
+    align-self: center;
+    text-align: center;
+  }
 `;
 
 const ButtonContainer = styled.div`
-width: 80%;
-align-self: flex-start;
+  width: 80%;
+  align-self: flex-start;
+  @media (max-width: 48em) {
+    align-self: center;
+    text-align: center;
+    button {
+      margin: 0 auto;
+    }
+  }
 `;
 
 const TypeWriterText = () => {
   return (
     <>
-    <Title>
+      <Title>
         Discover a new era of cool
         <Typewriter
-            options={{
-                autoStart:true,
-                loop:true,
-            }}
-            onInit={(typewriter) => {
-            typewriter.typeString('<span class="text-1"> NFTs.</span>')
-            .pauseFor(2000)
-            .deleteAll()
-            .typeString('<span class="text-2">Collectible Items.</span>')
-            .pauseFor(2000)
-            .deleteAll()
-            .typeString('<span class="text-3">Cool Foxes!</span>')
-            .pauseFor(2000)
-            .deleteAll()
-            .start()
-            }}
+          options={{
+            autoStart: true,
+            loop: true,
+          }}
+          onInit={(typewriter) => {
+            typewriter
+              .typeString('<span class="text-1"> NFTs.</span>')
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString('<span class="text-2">Collectible Items.</span>')
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString('<span class="text-3">Cool Foxes!</span>')
+              .pauseFor(2000)
+              .deleteAll()
+              .start();
+          }}
         />
-    </Title>
-    <SubTitle>Bored Of Apes? Try Something New.</SubTitle>
-    <ButtonContainer>
-    <Button text="Explore" link="#about" />
-    </ButtonContainer>
+      </Title>
+      <SubTitle>Bored Of Apes? Try Something New.</SubTitle>
+      <ButtonContainer>
+        <Button text="Explore" link="#about" />
+      </ButtonContainer>
     </>
+  );
+};
 
-  )
-}
-
-export default TypeWriterText
+export default TypeWriterText;
